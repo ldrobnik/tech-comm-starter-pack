46515e279165ea22a697a19ce7d9e1db807605db
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-06-06 01:59:03 +0200

    Change dark and light mode icons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
             // Dark/light switch icon options
             switchConfig: {
                 // Icon for the switch while in dark mode
-                darkIcon: '☾',
+                darkIcon: '🌙',
 
                 // CSS to apply to dark icon,
                 // React inline style object
@@ -36,7 +36,7 @@ module.exports = {
 
                 // Unicode icons such as '\u2600' will work
                 // Unicode with 5 chars require brackets: '\u{1F602}'
-                lightIcon: '🌣',
+                lightIcon: '☀️',
 
                 lightIconStyle: {
                     marginLeft: '1px',
